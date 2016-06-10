72c4515dccc4481f16f60885c88430323ed5d167
ztelur <ztelur@gmail.com>
2016-06-10 11:09:21 +0800

    oval button and animation

--- funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
+++ funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
@@ -9,20 +9,33 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
 
 /**
  * Created by homer on 16-6-9.
  */
 public class FunSwitch extends View {
     private final Paint mPaint = new Paint();
-    private final Path sPath = new Path();
+    private final Path mBackgroundPath = new Path();
     private int mWidth,mHeight;
-    private float sWidth,sHeight;
-    private float sLeft,sTop,sRight,sBottom;
-    private float sCenterX,sCenterY;
-
+    private float mBackgroundWidth, mBackgroundHeight;
+    private float mBackgroundLeft, mBackgroundTop, mBackgroundRight, mBackgroundBottom;
+    private float mScaleCenterX, mScaleCenterY;
+    private float mOvalButtonCenterX,mOvalButtonCenterY;
+    private float mOvalButtonRadius;
+    private float mOvalButtonStrokeWidth;
     private float mAnimationCount;
     private boolean mIsAnimationOn = false;
+    private final Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
+
+
+    private enum State {
+        STATE_SWITCH_ON,STATE_SWITCH_OFF,STATE_SWITCH_PENDING_ON,STATE_SWITCH_PENDING_OFF
+    }
+    private int  mState;
+
+
     public FunSwitch(Context context) {
         super(context);
     }
@@ -40,20 +53,27 @@ public class FunSwitch extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth = w;
         mHeight = h;
-        sLeft = sTop = 0;
-        sRight = mWidth;
-        sBottom  = mHeight * 0.8f;
-        sWidth = sRight - sLeft;
-        sHeight = sBottom - sTop;
-        sCenterX = (sRight + sLeft) / 2;
-        sCenterY = (sBottom + sTop) / 2;
+        mBackgroundLeft = mBackgroundTop = 0;
+        mBackgroundRight = mWidth;
+        mBackgroundBottom = mHeight * 0.8f;
+        mBackgroundWidth = mBackgroundRight - mBackgroundLeft;
+        mBackgroundHeight = mBackgroundBottom - mBackgroundTop;
+        mScaleCenterX = mBackgroundRight - mBackgroundBottom /2;
+        mScaleCenterY = mBackgroundTop + mBackgroundBottom /2;
 
-        RectF sRectF = new RectF(sLeft,sTop,sBottom,sBottom);
-        sPath.arcTo(sRectF,90,180);
-        sRectF.left = sRight - sBottom;
-        sRectF.right = sRight;
-        sPath.arcTo(sRectF,270,180);
-        sPath.close();
+        RectF sRectF = new RectF(mBackgroundLeft, mBackgroundTop, mBackgroundBottom, mBackgroundBottom);
+        mBackgroundPath.arcTo(sRectF,90,180);
+        sRectF.left = mBackgroundRight - mBackgroundBottom;
+        sRectF.right = mBackgroundRight;
+        mBackgroundPath.arcTo(sRectF,270,180);
+        mBackgroundPath.close();
+
+        // 白色按钮
+        mOvalButtonRadius = (mBackgroundBottom - mBackgroundTop) /2;
+        mOvalButtonCenterX = mBackgroundLeft + mOvalButtonRadius;
+        mOvalButtonCenterY = mBackgroundTop + mOvalButtonRadius;
+        mOvalButtonStrokeWidth = mOvalButtonRadius - mOvalButtonRadius * 0.9f;
+        mOvalButtonRadius *= 0.9f;
     }
 
     @Override
@@ -70,17 +90,27 @@ public class FunSwitch extends View {
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(0xffcccccc);
-        canvas.drawPath(sPath,mPaint); //绘制背景
+        canvas.drawPath(mBackgroundPath,mPaint); //绘制背景
 
-        if (mIsAnimationOn) {
-            mPaint.setColor(0xffffffff);
-            float scale = getScaleValue();
-            canvas.save();
-            canvas.scale(scale,scale,sCenterX,sCenterY);
-            canvas.drawPath(sPath,mPaint);
-            canvas.restore();
-            stepNextAnimationIfNeed();
-        }
+        mPaint.setColor(0xffffffff);
+        float scale = getScaleValue();
+        canvas.save();
+        canvas.scale(scale,scale, mScaleCenterX, mScaleCenterY);
+        canvas.drawPath(mBackgroundPath,mPaint);
+        canvas.restore();
+
+        //绘制按钮
+        mPaint.setColor(0xffffffff);
+        //绘制白色按钮的白色部分
+        canvas.translate(getTranslateValue(),0);
+        canvas.drawCircle(mOvalButtonCenterX,mOvalButtonCenterY,mOvalButtonRadius,mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(mOvalButtonStrokeWidth);
+        mPaint.setColor(0xffdddddd);
+        //绘制按钮的灰色边缘
+        canvas.drawCircle(mOvalButtonCenterX,mOvalButtonCenterY,mOvalButtonRadius,mPaint);
+        stepNextAnimationOrEndAnimation();
+
         mPaint.reset();
     }
 
@@ -99,18 +129,34 @@ public class FunSwitch extends View {
     }
 
     private void startAnimation() {
+
         mAnimationCount = 1f;
         mIsAnimationOn = true;
         invalidate();
     }
+
     private float getScaleValue() {
-        return 0.98f * (1f -mAnimationCount);
+        float value = mIsAnimationOn ? (0.98f * (1f -mAnimationCount)): 1;
+        return mInterpolator.getInterpolation(value);
     }
-    private void stepNextAnimationIfNeed() {
-        if (mAnimationCount >0) {
-            mAnimationCount -= 0.1f;
+
+    private float getTranslateValue() {
+        float currentValue = mIsAnimationOn ? (1f- mAnimationCount) : 0;
+        return mInterpolator.getInterpolation(currentValue) * (mBackgroundRight - mBackgroundBottom);
+
+    }
+
+    private void stepNextAnimationOrEndAnimation() {
+        if (mAnimationCount > 0 && mIsAnimationOn) {
+            mAnimationCount -= 0.05f;
             Log.e("TEST","call invalidate"+ mAnimationCount);
             invalidate();
+        } else {
+            mIsAnimationOn = false;
         }
+    }
+
+    public void setState(State state) {
+        mState = state.ordinal();
     }
 }
