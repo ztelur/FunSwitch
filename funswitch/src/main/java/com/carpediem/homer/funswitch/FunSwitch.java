eba148a0e901cf051e7e3f1a5a4b9e7f68c6565e
ztelur <ztelur@gmail.com>
2016-06-11 16:33:02 +0800

    iOS switch finish

--- funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
+++ funswitch/src/main/java/com/carpediem/homer/funswitch/FunSwitch.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,6 +20,8 @@ import android.view.animation.Interpolator;
 public class FunSwitch extends View {
     private final Paint mPaint = new Paint();
     private final Path mBackgroundPath = new Path();
+    private RectF mButtonRectF;
+    private float mButtonOffSet;
     private int mWidth,mHeight;
     private float mBackgroundWidth, mBackgroundHeight;
     private float mBackgroundLeft, mBackgroundTop, mBackgroundRight, mBackgroundBottom;
@@ -25,29 +29,36 @@ public class FunSwitch extends View {
     private float mOvalButtonCenterX,mOvalButtonCenterY;
     private float mOvalButtonRadius;
     private float mOvalButtonStrokeWidth;
-    private float mAnimationCount;
+    private float mAnimationPercent1;
+    private float mAnimationPercent2;
     private boolean mIsAnimationOn = false;
-    private final Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
-
+    private Interpolator mInterpolator = new AccelerateDecelerateInterpolator();
+    private boolean mIsCanClick = true; //动画期间无法点击
 
     private enum State {
         STATE_SWITCH_ON,STATE_SWITCH_OFF,STATE_SWITCH_PENDING_ON,STATE_SWITCH_PENDING_OFF
     }
-    private int  mState;
-
+    private State mState;
+    private State mLastState;
 
     public FunSwitch(Context context) {
         super(context);
+        init(context);
     }
 
     public FunSwitch(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public FunSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
+    private void init(Context context) {
+        mState = State.STATE_SWITCH_OFF;//初始化
+    }
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
@@ -74,6 +85,8 @@ public class FunSwitch extends View {
         mOvalButtonCenterY = mBackgroundTop + mOvalButtonRadius;
         mOvalButtonStrokeWidth = mOvalButtonRadius - mOvalButtonRadius * 0.9f;
         mOvalButtonRadius *= 0.9f;
+        mButtonOffSet = mOvalButtonRadius * 0.3f;
+        mButtonRectF = new RectF(mBackgroundLeft,mBackgroundTop,mBackgroundBottom,mBackgroundBottom);
     }
 
     @Override
@@ -101,19 +114,35 @@ public class FunSwitch extends View {
 
         //绘制按钮
         mPaint.setColor(0xffffffff);
+
         //绘制白色按钮的白色部分
         canvas.translate(getTranslateValue(),0);
-        canvas.drawCircle(mOvalButtonCenterX,mOvalButtonCenterY,mOvalButtonRadius,mPaint);
+        canvas.drawPath(figureButtonPath(),mPaint);
+
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(mOvalButtonStrokeWidth);
         mPaint.setColor(0xffdddddd);
         //绘制按钮的灰色边缘
-        canvas.drawCircle(mOvalButtonCenterX,mOvalButtonCenterY,mOvalButtonRadius,mPaint);
-        stepNextAnimationOrEndAnimation();
-
+        canvas.drawPath(figureButtonPath(),mPaint);
         mPaint.reset();
+        stepNextAnimationOrEndAnimation();
     }
 
+    private Path figureButtonPath() {
+        float percent = getButtonOffSetPercent();
+        float left = mOvalButtonCenterX - mOvalButtonRadius;
+        float top = mOvalButtonCenterY - mOvalButtonRadius;
+        float right = mOvalButtonCenterX + mOvalButtonRadius;
+        float bottom = mOvalButtonCenterY + mOvalButtonRadius;
+        Path buttonPath = new Path();
+        mButtonRectF = new RectF(left,top,right,bottom);
+        buttonPath.arcTo(mButtonRectF,90,180);
+        mButtonRectF.left += percent * mButtonOffSet;
+        mButtonRectF.right += percent * mButtonOffSet;
+        buttonPath.arcTo(mButtonRectF,270,180);
+        buttonPath.close();
+        return buttonPath;
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getDeviceId()) {
@@ -122,41 +151,107 @@ public class FunSwitch extends View {
             case MotionEvent.ACTION_CANCEL:
                 break;
             case MotionEvent.ACTION_UP:
-                startAnimation();
+                if (mIsCanClick) {
+                    startAnimation();
+                } else {
+                    Log.e("TEST", mIsCanClick + " ");
+                }
                 break;
         }
         return super.onTouchEvent(event);
     }
 
     private void startAnimation() {
-
-        mAnimationCount = 1f;
+        mLastState = mState;
+        if (State.STATE_SWITCH_OFF.equals(mState)) {
+            mState = State.STATE_SWITCH_PENDING_ON;
+            mAnimationPercent2 = 0.0f;
+        } else if (State.STATE_SWITCH_PENDING_ON.equals(mState)) {
+            mState = State.STATE_SWITCH_ON;
+            mAnimationPercent1 = 1.0f;
+        } else if (State.STATE_SWITCH_ON.equals(mState)) {
+            mState = State.STATE_SWITCH_PENDING_OFF;
+            mAnimationPercent2 = 0.0f;
+        } else if (State.STATE_SWITCH_PENDING_OFF.equals(mState)) {
+            mState = State.STATE_SWITCH_OFF;
+            mAnimationPercent1 = 0.0f;
+        } else {
+            throw new RuntimeException("no init state");
+        }
         mIsAnimationOn = true;
+        mIsCanClick = false;
+        mInterpolator = new AccelerateDecelerateInterpolator();
         invalidate();
     }
-
+    private float getButtonOffSetPercent() {
+        if (State.STATE_SWITCH_ON.equals(mState) || State.STATE_SWITCH_OFF.equals(mState)) {
+            return 0f;
+        } else if (State.STATE_SWITCH_PENDING_ON.equals(mState)) {
+            return mAnimationPercent2;
+        } else if (State.STATE_SWITCH_PENDING_OFF.equals(mState)) {
+            return mAnimationPercent2;
+        }
+        return 0f;
+    }
     private float getScaleValue() {
-        float value = mIsAnimationOn ? (0.98f * (1f -mAnimationCount)): 1;
+        float value = mIsAnimationOn ? (0.98f * (1f - mAnimationPercent1)): 1;
         return mInterpolator.getInterpolation(value);
     }
 
     private float getTranslateValue() {
-        float currentValue = mIsAnimationOn ? (1f- mAnimationCount) : 0;
+        if (State.STATE_SWITCH_PENDING_ON.equals(mState)) {
+            return 0f;
+        } else if (State.STATE_SWITCH_PENDING_OFF.equals(mState)) {
+            return (mBackgroundRight-mBackgroundBottom) - mButtonOffSet * getButtonOffSetPercent();
+        }
+        float currentValue = mIsAnimationOn ? (1f- mAnimationPercent1) : 0;
+        Log.e("TEST","currentValue is"+currentValue + " interpolator is"+ mInterpolator.getInterpolation(currentValue));
         return mInterpolator.getInterpolation(currentValue) * (mBackgroundRight - mBackgroundBottom);
-
     }
 
     private void stepNextAnimationOrEndAnimation() {
-        if (mAnimationCount > 0 && mIsAnimationOn) {
-            mAnimationCount -= 0.05f;
-            Log.e("TEST","call invalidate"+ mAnimationCount);
+        if (!mIsAnimationOn) {
+            return;
+        }
+        if (mAnimationPercent1 > 0 && mState.equals(State.STATE_SWITCH_ON)) {
+            mAnimationPercent1 -= 0.05f;
+            invalidate();
+        } else if (mAnimationPercent1 <=1.0f && mState.equals(State.STATE_SWITCH_OFF)){
+            mAnimationPercent1 += 0.05f;
+            invalidate();
+        } else if ((State.STATE_SWITCH_PENDING_ON.equals(mState) || State.STATE_SWITCH_PENDING_OFF.equals(mState))
+                            && mAnimationPercent2 < 1.0f) {
+            mAnimationPercent2 += 0.05f;
             invalidate();
         } else {
             mIsAnimationOn = false;
+            mIsCanClick = true;
+            mAnimationPercent2 = 0.0f;
         }
     }
 
     public void setState(State state) {
-        mState = state.ordinal();
+        mState = state;
     }
+
+
+//
+//    static final class SavedState extends BaseSavedState {
+//        private State mState;
+//
+//        public SavedState(Parcelable superState) {
+//            super(superState);
+//        }
+//
+//        public SavedState(Parcel source) {
+//            super(source);
+//            mState = State.values()[source.readInt()];
+//        }
+//
+//        @Override
+//        public void writeToParcel(Parcel out, int flags) {
+//            super.writeToParcel(out, flags);
+//            out.writeInt(mState.ordinal());
+//        }
+//    }
 }
